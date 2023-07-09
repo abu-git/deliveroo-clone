d39e9ec0c427a4b8be8ac878925cd456eaba2a53
Samuel Abu <abu.sammie@gmail.com>
2023-07-09 12:13:55 +0200

    categories started

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,9 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TextInput, ScrollView } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline";
+import { ChevronDownIcon, UserIcon, AdjustmentsVerticalIcon, MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import Categories from '../components/Categories';
 
 const HomeScreen = () => {
     const navigation = useNavigation();
@@ -15,10 +16,9 @@ const HomeScreen = () => {
     }, []);
 
     return (
-        <SafeAreaView>
+        <SafeAreaView className="bg-white pt-5">
             {/* Header */}
-            <View className="flex-row pb-3 items-center mx-4 space-x-2">
-                
+            <View className="flex-row pb-3 items-center mx-3 space-x-2">
                 <Image 
                     source={{
                         uri: 'https://links.papareact.com/wru'
@@ -37,7 +37,25 @@ const HomeScreen = () => {
 
                 <UserIcon className="" size={35} color="#00CCBB" />
             </View>
-            
+
+            {/* Search */}
+            <View className="flex-row items-center pb-2 mx-3 space-x-2">
+                <View className="flex-row space-x-2 bg-gray-200 p-3 flex-1">
+                    <MagnifyingGlassIcon color="gray" size={20} />
+                    <TextInput placeholder='Restaurants and Cuisines' keyboardType='default' />
+                </View>
+                
+                <AdjustmentsVerticalIcon color="#00CCBB" />
+            </View>
+
+            {/* Body */}
+            <ScrollView className="bg-gray-100" contentContainerStyle={{ paddingBottom: 100 }}>
+                {/* Categories */}
+                <Categories />
+
+
+                {/* Featured Rows */}
+            </ScrollView>
         </SafeAreaView>
     )
 }
