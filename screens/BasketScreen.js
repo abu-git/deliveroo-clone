a96ec7044e916128f080a0b018806f33ce6f2063
Samuel Abu <abu.sammie@gmail.com>
2023-07-22 01:07:08 +0200

    basket screen progress, delivery screen started

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,16 +1,18 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity, Image, ScrollView } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import React, { useMemo, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { useDispatch, useSelector } from 'react-redux'
 import { selectRestaurant } from '../features/restaurantSlice'
-import { selectBasketItems } from '../features/basketSlice'
+import { removeFromBasket, selectBasketItems, selectBasketTotal } from '../features/basketSlice'
 import { XCircleIcon } from 'react-native-heroicons/solid'
+import { urlFor } from '../sanity'
 
 const BasketScreen = () => {
     const navigation = useNavigation()
     const restaurant = useSelector(selectRestaurant)
     const items = useSelector(selectBasketItems)
+    const basketTotal = useSelector(selectBasketTotal)
     const [groupedItemsInBasket, setGroupItemsInBasket] = useState([])
     const dispatch = useDispatch()
 
@@ -23,7 +25,7 @@ const BasketScreen = () => {
         setGroupItemsInBasket(groupedItems)
     }, [items])
 
-    console.log(groupedItemsInBasket)
+    //console.log(groupedItemsInBasket)
 
     return (
         <SafeAreaView className="flex-1 bg-white">
@@ -52,6 +54,51 @@ const BasketScreen = () => {
                     <Text className="flex-1 text-black">Deliver in 40 - 45 min</Text>
                     <TouchableOpacity>
                         <Text className="text-[#00CCBB]">Change</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <ScrollView className="divide-y divide-gray-200">
+                    {Object.entries(groupedItemsInBasket).map(([key, items]) => (
+                        <View key={key} className="flex-row items-center space-x-3 bg-white py-2 px-5">
+                            <Text>{items.length} x</Text>
+                            <Image 
+                                source={{ uri: urlFor(items[0]?.image).url() }}
+                                className="h-12 w-12 rounded-full"
+                            />
+                            <Text className="flex-1">{items[0]?.name}</Text>
+
+                            <Text className="text-gray-600">R{items[0]?.price}</Text>
+
+                            <TouchableOpacity>
+                                <Text
+                                    className="text-[#00CCBB] text-xs"
+                                    onPress={() => dispatch(removeFromBasket({id: key}))}
+                                >
+                                    Remove
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    ))}
+                </ScrollView>
+
+                <View className="p-5 bg-white mt-5 space-y-4">
+                    <View className="flex-row justify-between">
+                        <Text className="text-gray-400">SubTotal</Text>
+                        <Text className="text-gray-400">R{basketTotal}</Text>
+                    </View>
+
+                    <View className="flex-row justify-between">
+                        <Text className="text-gray-400">Delivery Fee</Text>
+                        <Text className="text-gray-400">R20</Text>
+                    </View>
+
+                    <View className="flex-row justify-between">
+                        <Text className="text-black">Order Total</Text>
+                        <Text className="text-black font-extrabold">R{basketTotal + 20}</Text>
+                    </View>
+
+                    <TouchableOpacity onPress={() => navigation.navigate("PreparingOrder")} className="rounded-lg bg-[#00CCBB] p-4">
+                        <Text className="text-center text-white text-lg font-bold">Place Order</Text>
                     </TouchableOpacity>
                 </View>
             </View>
---
+++ screens/PreparingOrderScreen.js
@@ -0,0 +1,39 @@
+import { View, Text, Image } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNavigation } from '@react-navigation/native'
+import React, { useEffect } from 'react'
+import * as Animatable from 'react-native-animatable'
+import * as Progress from 'react-native-progress'
+
+const PreparingOrderScreen = () => {
+    const navigation = useNavigation()
+
+    useEffect(() => {
+        setTimeout(() => {
+            navigation.navigate("Delivery")
+        }, 4000)
+    }, [])
+
+    return (
+        <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center">
+            <Animatable.Image
+                source={require("../assets/food2.gif")}
+                animation="slideInUp"
+                iterationCount={1}
+                className="w-60 h-60"
+            />
+
+            <Animatable.Text
+                animation="slideInUp"
+                iterationCount={1}
+                className="text-lg my-10 text-white font-bold text-center"
+            >
+                Waiting for Restaurant to accept your order!
+            </Animatable.Text>
+
+            <Progress.Circle size={60} indeterminate={true} color='white' />
+        </SafeAreaView>
+    )
+}
+
+export default PreparingOrderScreen
