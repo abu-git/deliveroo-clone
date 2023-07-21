78e3079a5391fb95ceb574112d600dd760879b90
Samuel Abu <abu.sammie@gmail.com>
2023-07-21 14:00:19 +0200

    redux progress

--- components/DishRow.js
+++ components/DishRow.js
@@ -3,12 +3,12 @@ import React,{ useState } from 'react'
 import { urlFor } from '../sanity';
 import { MinusCircleIcon, PlusCircleIcon } from 'react-native-heroicons/solid';
 import { useDispatch, useSelector } from 'react-redux';
-import { addToBasket, selectBasketItems } from '../features/basketSlice';
+import { addToBasket, selectBasketItems, selectBasketItemsWithId } from '../features/basketSlice';
 
 
 const DishRow = ({ id, name, description, price, image}) => {
     const [isPressed, setIsPressed] = useState(false);
-    const items = useSelector(selectBasketItems);
+    const items = useSelector((state) => selectBasketItemsWithId(state, id));
     const dispatch = useDispatch();
 
     const addItemToBasket = () => {
